74840406b6c51d25075dd9c45d7c7c420344fc59
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-01-22 02:58:20 -0300

    setup files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
+import { AppRouter } from '../src/routers/AppRouter'
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles'
+
+let theme = createTheme({
+    typography: {
+        fontFamily: 'Public Sans',
+    },
+})
+
+// automatically change font size
+theme = responsiveFontSizes(theme)
+
 function App() {
-    return <h1>hola</h1>
+    return (
+        <ThemeProvider theme={theme}>
+            <AppRouter />
+        </ThemeProvider>
+    )
 }
 
 export default App
