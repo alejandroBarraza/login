fd4032c2b772fd92f2f1fcdaa13ba351e6f3dcc9
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-02-03 18:48:47 -0300

    redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import authReducer from '../features/auth/authSlice'
+import { authApi } from './services/auth'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        [authApi.reducerPath]: authApi.reducer,
+        auth: authReducer,
+    },
+
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 })
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
