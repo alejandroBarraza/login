36a67edf5f601c41852075bec67b564e5a73798e
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-02-03 18:47:35 -0300

    redux slice auth

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    username: null,
+    isLogged: false,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.username = action.payload.username
+            state.isLogged = true
+        },
+        removeUser: (state) => {
+            state.nombre = null
+            state.isLogged = false
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { setUser, removeUser } = authSlice.actions
+
+export default authSlice.reducer
+
+// current user
+export const selectUser = (state) => state.auth.username
+export const isLogged = (state) => state.auth.isLogged
