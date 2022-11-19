a4da9410bfc582417efb99cfdadfeed620666484
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2022-11-19 16:12:27 +0900

    api deploymeny

--- src/app/services/auth.js
+++ src/app/services/auth.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: '/api/auth/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_PUBLIC_URL}/api/auth/` }),
 
     endpoints: (builder) => ({
         registerUser: builder.mutation({
